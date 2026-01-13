ac86910f42c06fcac07e35b4a53cc944d34afe12
Miguel Sietereales <miguel.sietereales@gmail.com>
2026-01-13 17:45:15 +0800

    fix: favico

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
@@ -45,6 +46,17 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: `./public/index.html`,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./public",
+          to: "",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
     }),
   ],
   optimization: {
