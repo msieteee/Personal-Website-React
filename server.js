57ce53d2db37c6eb8f89836960a5b13905104bba
Miguel Sietereales <miguel.sietereales@gmail.com>
2026-01-13 14:53:33 +0800

    fix: ssl certs

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Force HTTPS in production
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      // Redirect HTTP to HTTPS
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+}
+
 // Serve the static files from the "build" folder
 app.use(express.static(path.join(__dirname, "build")));
 
