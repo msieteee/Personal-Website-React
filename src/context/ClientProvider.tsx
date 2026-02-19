dba22f728e482cd91d0c6e748292ebb8b56b134f
Miguel Sietereales <miguel.sietereales@gmail.com>
2026-02-19 16:47:32 +0800

    fix: add missing use client tag

--- src/context/ClientProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import { HomeProvider } from "./HomeProvider";
-
-const ClientProvider = ({ children }: { children: ReactNode }) => {
-  return <HomeProvider>{children}</HomeProvider>;
-};
-
-export default ClientProvider;
